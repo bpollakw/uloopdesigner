3a51c0e4d34fd6aa0c51e10f2d3e931b1e21eb7c
Bernardo Pollak <bpollakw@gmail.com>
2019-05-14 21:59:21 -0400

    update schema

--- initialise.py
+++ initialise.py
@@ -2,4 +2,6 @@ from loopDB import *
 import config
 
 loopDB = LoopDB( config.DATABASE_URL, clean = True  )
+#loopDB = LoopDB( "postgres:///uloop", clean = True  )
 loopDB.initFromFile( config.SCHEMA_PATH )
+
