567ede18fb496896febe369dd8bd7e1006480a02
Bernardo Pollak <bpollakw@gmail.com>
2019-05-27 14:08:47 -0400

    roolback

--- static/js/forms/assemblyForm.js
+++ static/js/forms/assemblyForm.js
@@ -118,6 +118,9 @@ function assemblyForm(backboneList){
 					if (w2ui['sideBar'].parts.find( ({name}) => name == this.record["Part name"] )){
 						event.errors.push( {field: this.get("Part name"), error: "Part name already exists"} )
 					}
+					else(this.record["Part name"].length > 99){
+						event.errors.push( {field: this.get("Part name"), error: "Part name length too long"} )
+					}
 				};
 
 				if (this.validate(true).length == 0){
