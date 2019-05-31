2ba0d4af7085600e753936a318b0e155e223f571
Bernardo Pollak <bpollakw@gmail.com>
2019-05-31 16:09:37 -0400

    heroku config

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 DEBUG			= True
-SECRET_KEY		= "secret"
-DATABASE_URL	= "postgres:///loopdb"
+SECRET_KEY		= "HAKSJDH*@QE*Q@EUQD?<?><?><@23123HN(*@YEHQ#JKBED(*XYIH"
+DATABASE_URL	= "postgres://xccsnwkwlfvsbg:4e3906e0aa6f0dfc05074677206f8fedba9ed1208a5d7d07032f6c603ca229f8@ec2-50-17-246-114.compute-1.amazonaws.com:5432/d6jpn9esrqkhcu"
 SCHEMA_PATH		= "schema.json"
